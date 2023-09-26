b3fa4a1f651aa2d5817614595437229c18e83341
dlacrzz <122063410+dlacrzz@users.noreply.github.com>
2023-09-25 17:47:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ var youtubeButton = document.getElementById("youtube");
 youtubeButton.addEventListener("click", function() {
     window.location.href = "https://www.youtube.com/watch?v=rWH4XGO1KSU";
 });
+
+
+const toggleButton = document.getElementById("toggleButton");
+const footer = document.querySelector(".footer");
+
+toggleButton.addEventListener("click", function() {
+    if (footer.style.display === "none" || footer.style.display === "") {
+        footer.style.display = "block";
+    } else {
+        footer.style.display = "none";
+    }
+});
